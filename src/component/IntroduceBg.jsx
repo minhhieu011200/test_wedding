922aa9bc5a868a55bf082e05e2162eab5cf4d158
Minh Hieu <minhhieu011200@gmail.com>
2025-01-25 21:59:57 +0700

    thay doi anh bia

--- src/component/IntroduceBg.jsx
+++ src/component/IntroduceBg.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import bgIntroduce from '../assets/img/bg-introduce-2.png';
 import test from '../assets/img/test.jpg';
+import { ReactComponent as MyIcon } from '../assets/img/test.svg';
+// import myIcon from '../assets/img/test.svg';
+
 import './IntroduceBg.css'
 function IntroduceBg(props) {
     return (
         <div className='bg-introduce'>
-            <img className='background' src={bgIntroduce} />
-            <div class="foreground-wrapper">
-                <img className='foreground animate__animated animate__jackInTheBox' src={test} />
+            <MyIcon className='background' />
+            {/* <img className='background' src={myIcon} /> */}
+            <div className="foreground-wrapper">
+                <div className='animate__animated animate__jackInTheBox'>
+
+                    <img className='foreground ' src={test} />
+                </div>
             </div>
 
 
